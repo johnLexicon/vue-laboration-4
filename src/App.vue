92520d56afcf5695bab3916f5dabc6cb72691778
john lundgren <john.lundgren.lexicon@gmail.com>
2021-05-28 09:22:21 +0200

    Added bg image to Home page.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <template>
   <div>
-    <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div> -->
     <Navbar />
-    <router-view />
+    <div>
+      <router-view />
+    </div>
   </div>
 </template>
 <script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="home">
-    <h1>Home Page</h1>
-  </div>
+  <div class="home"></div>
 </template>
+<style scoped>
+.home {
+  height: 100vh;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+    url("https://images.unsplash.com/photo-1567924675637-283a6742993e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1490&q=80");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+</style>
