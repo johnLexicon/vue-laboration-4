cd073881ecdf8327d4a2e41e3c7f74e5f5bef262
john lundgren <john.lundgren.lexicon@gmail.com>
2021-05-30 09:34:05 +0200

    Added getter.

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,17 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    admin: null,
+    users: [],
   },
-  mutations: {
+  getters: {
+    signedIn: (state) => state.admin !== null,
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  mutations: {},
+  actions: {},
+  modules: {},
+});
