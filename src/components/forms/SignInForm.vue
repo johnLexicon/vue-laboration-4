01a860d2b2cfc17e7997031f06559a49a130f784
john lundgren <john.lundgren.lexicon@gmail.com>
2021-05-31 14:02:16 +0200

    Added Sign in logic.

--- src/components/forms/SignInForm.vue
+++ src/components/forms/SignInForm.vue
@@ -1,11 +1,12 @@
 <template>
-  <form>
+  <form class="content" v-on:submit.prevent="signIn">
     <!-- Email input -->
     <div class="form-outline mb-5">
       <input
         type="email"
         id="email"
         class="form-control text-light border-bottom border-light"
+        v-model="email"
       />
       <label class="form-label text-light" for="email">Email address</label>
     </div>
@@ -16,11 +17,16 @@
         type="password"
         id="password"
         class="form-control border-bottom border-light text-light"
+        v-model="password"
       />
       <label class="form-label text-light" for="password">Password</label>
     </div>
-    <div class="alert text-danger border border-danger" role="alert">
-      A simple light alert—check it out!
+    <div
+      v-if="error"
+      class="alert text-danger border border-danger"
+      role="alert"
+    >
+      {{ error }}
     </div>
     <!-- Submit button -->
     <button type="submit" class="btn btn-outline-light btn-block">
@@ -30,5 +36,39 @@
 </template>
 
 <script>
-export default {};
+import { mapActions, mapGetters } from "vuex";
+export default {
+  name: "SignInForm",
+  data() {
+    return {
+      error: "",
+      email: "",
+      password: "",
+    };
+  },
+  computed: {
+    ...mapGetters(["signedIn"]),
+  },
+  methods: {
+    ...mapActions(["signInAdmin"]),
+    async signIn() {
+      this.error = "";
+      if (!this.email || !this.password) {
+        this.error = "Email and Password are mandatory";
+        return;
+      }
+      await this.signInAdmin({ email: this.email, password: this.password });
+      if (this.signedIn) {
+        this.$router.replace("/users");
+      } else {
+        this.error = "Authentication failed";
+      }
+    },
+  },
+};
 </script>
+<style scoped>
+.content {
+  margin-top: -58px; /* Nav bar height */
+}
+</style>
