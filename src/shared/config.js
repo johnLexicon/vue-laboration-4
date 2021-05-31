3ad1f0fdf0c931130921d4c52ddc972e034f39b5
John Lundgren <john.lundgren.lexicon@gmail.com>
2021-05-31 07:01:04 +0200

    Added config file.

---
+++ src/shared/config.js
@@ -0,0 +1,8 @@
+const host =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000'
+    : 'https://lexicon-express-users.herokuapp.com/api';
+export const API = {
+  users: `${host}/users`,
+  admins: `${host}/users`,
+};
